007710181cbde8b7c5fe4262d7dc3f67774f3ba9
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2024-08-01 17:23:13 +0800

    added *src = NULL in push function so that stack is set to NULL after it pushes its last node. this solved the infinite loop issue in sort_stacks while (*b) loop

--- srcs/commands/push.c
+++ srcs/commands/push.c
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 13:18:23 by etien             #+#    #+#             */
-/*   Updated: 2024/08/01 16:55:36 by etien            ###   ########.fr       */
+/*   Updated: 2024/08/01 17:21:18 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 // A pointer is created to the node to be pushed because it has to be
 // detached from the src stack and moved to the dst stack.
 // The src pointer is updated to the next node.
+// IMPORTANT: If there is no next node, the src pointer will be set to NULL.
+// Without this line, there will be an infinite loop when pushing from
+// stack B back to stack A.
 // The push node will become the new head of the dst stack.
 static void	push(t_stack_node **dst, t_stack_node **src)
 {
@@ -31,6 +34,8 @@ static void	push(t_stack_node **dst, t_stack_node **src)
 		*src = (*src)->next;
 		(*src)->prev = NULL;
 	}
+	else
+		*src = NULL;
 	if (!*dst)
 	{
 		push_node->next = NULL;
